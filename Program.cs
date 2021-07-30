8e9f7c55fed17c06f8bb615670a4e96bb47846f2
Denis <trueman.dubna@gmail.com>
2021-07-30 12:42:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		string name = "jane";
+		int age = 27;
+		bool pet = true;
+		double size = 37.5;
+
+		Console.WriteLine("Как вас зовут? " + name);
+		Console.WriteLine("Сколько Вам лет? "+ age);
+		Console.WriteLine("Есть ли у Вас питомец " + pet);
+		Console.WriteLine("Какой у Вас размер ноги " +  size);
+		Console.Read();
+
+	}
+}
